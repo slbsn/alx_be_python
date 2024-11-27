87ae27577f869b139aa95cd5dcacb0748a7d9e7c
Ndumiso Tyam <138490666+slbsn@users.noreply.github.com>
2024-11-27 18:17:13 +0200

    Create simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+principal: The initial amount of money (e.g., $1000).
+principal=1000
+rate: The annual interest rate in decimal form (e.g., 5% as 0.05).
+rate=0.05
+time: The duration the money is invested or borrowed for, in years.
+time=3
+interest = principal * rate * time
+# Print the result
+print("The simple interest is:", interest)
