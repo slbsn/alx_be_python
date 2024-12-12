f36344f5a1915dd93ab9404e4cf593ecf69f116f
Ndumiso Tyam <138490666+slbsn@users.noreply.github.com>
2024-12-12 23:10:04 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,3 +1,20 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if self.__account_balance >= amount:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance:.2f}")
+
 import sys
 from bank_account import BankAccount
 
